6d32dd0364cf7c54bcb006685af1f0431fa3cc73
andmccutchan <andmccutchan2004@gmail.com>
2025-02-06 20:17:24 -0500

    Updated user schema model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String },
     password: { type: String, required: true },
 });
 
