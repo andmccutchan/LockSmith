ee0f7ee7e5230901fd3114d15f76294d877577bc
andmccutchan <andmccutchan2004@gmail.com>
2025-02-14 10:43:09 -0500

    Made footer clear

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-dark text-light text-center py-3 mt-auto">
+    <footer className=" text-light text-center py-3 mt-auto">
       <div className="container">
         <p className="mb-0">&copy; {new Date().getFullYear()} LockSmith | Developed by Andrew McCutchan</p>
       </div>
