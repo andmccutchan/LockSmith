19e1a3ed568bd9a3cb91a22e37a15bb9c79a9c02
andmccutchan <andmccutchan2004@gmail.com>
2025-02-24 14:37:40 -0500

    never mind that command was depricated

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,7 @@ app.use(express.json()); //Parse Json Data
 
 //Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
